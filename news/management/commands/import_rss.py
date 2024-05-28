8e55c2b21ce990dd99be2360346a119049499226
haiderali1716 <haiderjutt1716@gmail.com>
2024-05-28 14:05:18 +0500

    add category

--- news/management/commands/import_rss.py
+++ news/management/commands/import_rss.py
@@ -10,21 +10,22 @@ class Command(BaseCommand):
     help = 'Import RSS feed entries into the NewsArticle model'
 
     def handle(self, *args, **kwargs):
-        rss_urls = [
-            "https://feeds.bbci.co.uk/news/rss.xml",
-            "https://rss.cnn.com/rss/edition.rss",
-            "https://www.thenews.com.pk/rss/1/1",
-            "https://www.thenews.com.pk/rss/1/8",
-            "https://www.thenews.com.pk/rss/2/14",
-            "https://feeds.bbci.co.uk/news/world/rss.xml",
-            "https://feeds.bbci.co.uk/news/business/rss.xml",
-            "https://feeds.bbci.co.uk/news/health/rss.xml",
-            # Add more RSS feed URLs here
-        ]
+        rss_urls = {
+            "https://feeds.bbci.co.uk/news/rss.xml": "general",
+            "https://rss.cnn.com/rss/edition.rss": "general",
+            "https://www.yahoo.com/news/rss": "general",
+            "https://www.thenews.com.pk/rss/1/1": "general",
+            "https://www.thenews.com.pk/rss/1/8": "general",
+            "https://www.thenews.com.pk/rss/2/14": "general",
+            "https://feeds.bbci.co.uk/news/world/rss.xml": "world",
+            "https://feeds.bbci.co.uk/news/business/rss.xml": "business",
+            "https://feeds.bbci.co.uk/news/health/rss.xml": "health",
+            # Add more RSS feed URLs and their categories here
+        }
 
         home_page = Page.objects.get(slug='home')  # Ensure you have a home page
 
-        for rss_url in rss_urls:
+        for rss_url, category in rss_urls.items():
             feed = feedparser.parse(rss_url)
             entries = feed.entries
 
@@ -45,10 +46,10 @@ class Command(BaseCommand):
 
                 if news_article:
                     # Update existing article
-                    self.stdout.write(f"Article already exists: {title}")
                     news_article.link = link
                     news_article.description = summary
                     news_article.published_date = published_date
+                    news_article.category = category
 
                     if image_url:
                         image_content = save_image_from_url(image_url, title, f"{slugify(title)}.jpg")
@@ -62,15 +63,14 @@ class Command(BaseCommand):
                     news_article.save()
                 else:
                     # Create new article
-                    self.stdout.write(f"Added news article: {title}")
-                    slug = f"{slugify(title)}-{home_page.id}-{NewsArticle.objects.count()}"
                     news_article = NewsArticle(
                         title=title,
-                        slug=slug,
+                        slug=slugify(title),
                         news_title=title,
                         link=link,
                         description=summary,
                         published_date=published_date,
+                        category=category,
                     )
 
                     if image_url:
--- news/models.py
+++ news/models.py
@@ -5,13 +5,18 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.admin.panels import PageChooserPanel
 from wagtail.images.models import Image
 
-
 class NewsArticle(Page):
     published_date = models.DateField()
     news_title = models.CharField(max_length=255)
     description = models.TextField()
     link = models.URLField()
     image_url = models.ImageField(upload_to='images/', null=True, blank=True)
+    category = models.CharField(max_length=50, choices=[
+        ('business', 'Business'),
+        ('world', 'World'),
+        ('health', 'Health'),
+        ('general', 'General'),
+    ], default='general')
 
     content_panels = Page.content_panels + [
         FieldPanel('published_date'),
@@ -19,4 +24,5 @@ class NewsArticle(Page):
         FieldPanel('description'),
         FieldPanel('link'),
         FieldPanel('image_url'),
+        FieldPanel('category'),
     ]
